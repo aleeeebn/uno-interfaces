8e546a3b6aba7bd7c70fb8c4224025ad5e0dbb06
Llanazzz <angeliusparter@gmail.com>
2026-09-24 11:45:22 -0600

    Commit de prueba

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -14,6 +14,8 @@ namespace WindowsFormsApp1
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            MessageBox.Show("sixsevenaldo");
+
         }
     }
 }
