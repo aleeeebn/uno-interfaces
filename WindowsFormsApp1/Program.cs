f3201b61c953aba6fbc66567a01a65d3bb2887fe
Llanazzz <angeliusparter@gmail.com>
2026-09-24 11:42:14 -0600

    lll

--- WindowsFormsApp1/Program.cs
+++ WindowsFormsApp1/Program.cs
@@ -8,12 +8,9 @@ namespace WindowsFormsApp1
 {
     internal static class Program
     {
-        /// <summary>
-        /// Punto de entrada principal para la aplicación.
-        /// </summary>
-        [STAThread]
         static void Main()
         {
+            MessageBox.Show("Hola");
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
